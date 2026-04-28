c577b800be2dc823e4a3be0fbc02a338996b8287
Niti <154505008+Nitisakjaiya@users.noreply.github.com>
2026-04-28 14:52:20 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb", // 👈 เพิ่มบรรทัดนี้เพื่อขยายเป็น 10MB (หรือตามที่ต้องการ)
+    },
+  },
 };
 
 export default nextConfig;
